c28562512a38343de6735fcf491078bb294881c6
Rhian Castro <rhi.castro@gmail.com>
2020-03-16 23:15:20 -0300

    teste

--- helpers/reddit.py
+++ helpers/reddit.py
@@ -4,3 +4,13 @@ from helpers.commonConfig import CommonConfig
 reddit = praw.Reddit(client_id=CommonConfig.CLIENT_ID,
                      client_secret=CommonConfig.CLIENT_SECRET,
                      user_agent=CommonConfig.AGENT_USER)
+
+
+def getSub():
+    pass
+
+def getUser():
+    pass
+
+def getUserAndType():
+    pass
